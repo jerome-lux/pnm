985d92f13e481465500ef4bd2804afeea57fec42
jlux <jerome.lux78@gmail.com>
2023-09-29 15:00:00 +0200

    formatting

--- pnm/__init__.py
+++ pnm/__init__.py
@@ -1,6 +1,6 @@
 from pnm.PNM import pore_network
 from pnm.Generators import *
-from pnm.Visualization import PNM_3d_viewer,Fast3DViewer,vtk_viewer
+from pnm.Visualization import PNM_3d_viewer, Fast3DViewer, vtk_viewer
 from pnm.Stats import *
 from pnm.Transformation import *
 from pnm.Algorithms.MDiff import molecular_diffusion, molecular_diffusion_deprecated
